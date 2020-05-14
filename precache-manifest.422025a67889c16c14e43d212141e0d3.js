5b065adf6a33ddb505c3d5153602563e237fa75b
Juan <quinterosp.juan@gmail.com>
2020-05-13 21:39:58 -0300

    Updates

---
+++ precache-manifest.422025a67889c16c14e43d212141e0d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae537fd30129e6639a432341d6d428f8",
+    "url": "/chouza-index/index.html"
+  },
+  {
+    "revision": "0aeeef5814c0af611f41",
+    "url": "/chouza-index/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fdb1ee08e9540b5e94c6",
+    "url": "/chouza-index/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0aeeef5814c0af611f41",
+    "url": "/chouza-index/static/js/2.5ecb4b15.chunk.js"
+  },
+  {
+    "revision": "777db246545e57363857b884a88741bf",
+    "url": "/chouza-index/static/js/2.5ecb4b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb1ee08e9540b5e94c6",
+    "url": "/chouza-index/static/js/main.b7557015.chunk.js"
+  },
+  {
+    "revision": "d90d8abc93ad02920457",
+    "url": "/chouza-index/static/js/runtime-main.4ba1094e.js"
+  }
+]);
